b3fe9f560a761c9439a8dfbc9dc71e2bfdc27dc4
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2018-11-01 18:31:03 +0300

    added multiple_password

--- password.py
+++ password.py
@@ -5,9 +5,15 @@ class Password:
 
     Password_list = []
 
-    def _init_(self,first_name,last_name,password):
+    def __init__(self,first_name,last_name,email,password):
         self.first_name = first_name
         self.last_name = last_name
+        self.email = email
         self.password = password
+    def save_password(self):
+        """
+        save_password method saves pasword objects into password_list
+        """
+        Password.password_list.append(self)
 
 
--- password_test.py
+++ password_test.py
@@ -7,19 +7,26 @@ class TestPassword(unittest.TestCase):
 
     Args:
         unitest.TestCase: TestCase class that helps in creating test cases
-    """
-def setUp(self):
-    """
-    Set up method to run before each test cases.
-    """
-    self.new_password = Password("first_name","last_name","password")
-def test_init(self):
-    """
-    test_init test case to test if the object is initialized properly
-    """
-    self.assertEgual(self.new_password.first_name,"",)
-    self.assertEqual(self.new_password.last_name,"",)
-    self.assertEqual(self.new_password.password,"",)
+        """
+    def setUp(self):
+        """
+        Set up method to run before each test cases.
+        """
+        self.new_password = Password("first_name","last_name","email","password")
+    def test_init(self):
+        """
+        test_init test case to test if the object is initialized properly
+        """
+        self.assertEqual(self.new_password.first_name,"first_name",)
+        self.assertEqual(self.new_password.last_name,"last_name",)
+        self.assertEqual(self.new_password.email,"email",)
+        self.assertEqual(self.new_password.password,"password",)
+
+    def test_save_password(self):
+        """
+        test_save_password.save_password()
+        self.assertEqual(len(Password.password_list),1)
+        """
 
 if __name__ == '__main__':
     unittest.main()
