531bb46ed5f79eef1c758c2aec6b646af98d091c
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2018-11-02 10:56:52 +0300

    addeddelete function

--- password.py
+++ password.py
@@ -3,23 +3,28 @@ class Password:
     clas that generates new instances of password
     """
 
-    Password_list = []
+    password_list = []
 
     def __init__(self,first_name,last_name,email,password):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.password = password
+
     def save_password(self):
         """
-        save_password method saves pasword objects into password_list
+        save_password method saves password objects into password_list
         """
+
         Password.password_list.append(self)
-    def test_save_multiple_password(self)
+
+    def delete_password(self):
         """
-        test_save_multiple_password to check if we can save multiple password to our password_list
+        delete_password method deletes a saved password from the password_list
         """
-        self.new_password.save_password()
-        test_password = Password("Test","user")
+        Password.password_list.remove(self)
+
+    
+
 
 
--- password_test.py
+++ password_test.py
@@ -17,7 +17,6 @@ class TestPassword(unittest.TestCase):
         """
         test_init test case to test if the object is initialized properly
         """
-        self.assertEqual(self.new_password.site_name,'imgur')
         self.assertEqual(self.new_password.first_name,"first_name",)
         self.assertEqual(self.new_password.last_name,"last_name",)
         self.assertEqual(self.new_password.email,"email",)
@@ -25,9 +24,41 @@ class TestPassword(unittest.TestCase):
 
     def test_save_password(self):
         """
-        test_save_password.save_password()
-        self.assertEqual(len(Password.password_list),1)
+        test_save_password test case to test if the password object is savedinto the password list
         """
+        self.new_password.save_password()
+        self.assertEqual(len(Password.password_list),1)
+
+    def tearDown(self):
+        """
+        tearDown method that does clean up after each test case has run.
+        """
+        Password.password_list = []
+
+    def test_save_multiple_password(self):
+        """
+        test_save_multiple_password to check if we password save multiple password object to our multiple password
+        """
+        self.new_password.save_password()
+        test_password = Password("Test","user","email","password")
+        test_password.save_password()
+        self.assertEqual(len(Password.password_list),2)
+
+    def test_delete_password(self):
+        """
+        test_delete_password to test if we can remove a password from password list
+        """
+        self.new_password.delete_password()
+        
+
+
+
+
+
+
+    
+
+
 
 if __name__ == '__main__':
     unittest.main()
